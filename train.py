c0ced82f2bc27e6b8eff7171f8bc0cf10f2ba017
Renan Santos <renansantosmendes@gmail.com>
2022-12-11 20:49:47 -0300

    add source code

---
+++ train.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+import numpy as np
+import random
+
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras import Sequential
